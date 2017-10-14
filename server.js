d12bb0e422255fa0c1529f8666d91c6bda5d282e
Martin Gao <martinigao88@yahoo.com>
2017-10-14 05:12:24 -0400

    Allow passing menu

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const app = express();
 const http = require('http').Server(app);
 app.use(bodyParser.urlencoded({extended: true}));
 
-
 var mongoUri = 'mongodb://localhost/foodalert';
 var MongoClient = require('mongodb').MongoClient, format = require('util').format;
 
@@ -17,6 +16,25 @@ var db = MongoClient.connect(mongoUri, function(err, databaseConnection) {
 });
 
 app.use(express.static('public'));
+// Add headers
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 
 
 app.get('/', function(req, res) {
@@ -26,6 +44,11 @@ app.get('/', function(req, res) {
 
 app.get('/menu', function(req, res) {
     res.sendFile('menu.json', {root: __dirname});
+});
+
+app.post('/submit', function(req, res) {
+    // where the data is actually stored
+    req.body
 });
 
 http.listen(3000, function() {
